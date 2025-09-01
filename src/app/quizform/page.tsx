493f25f918148419f41a60d5ab9cefe4c11dc10b
Aarush0-5 <singhaar0110@gmail.com>
2025-09-02 01:37:14 +0530

    Quiz page live again!

--- src/app/quizform/page.tsx
+++ src/app/quizform/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState, useEffect, FormEvent} from "react";
+import {useState, useEffect} from "react";
 import axios, { AxiosError } from 'axios';
 import {useRouter} from "next/navigation"
 
