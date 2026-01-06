8a24db746bb638f185eb523cec1e24ec3482cd92
Aarush0-5 <singhaar0110@gmail.com>
2026-01-06 21:55:23 +0530

    ga id added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +40,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-EY5VNFQ0X2" />
         {children}
       </body>
     </html>
